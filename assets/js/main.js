e470f41c99438686d4985ca68143161117621553
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-11-01 14:02:47 -0300

    estilo e index finalizados

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let pergunta = document.querySelector(".pergunta")
+let resposta = [...document.querySelectorAll(".resposta")]
+let confirma = document.getElementById("confirma")
+
+const radio = [...document.querySelectorAll(".radio")]
+const btn_enviar = document.getElementById("enviar")
+const btn_next = document.getElementById("next")
+
